92b9f0cf98b499b079c305ade8c909ca8158e6db
Timothy_Su <hantingsu@gmail.com>
2024-07-20 17:01:29 +0800

    practice use emum and switch to implement mill-merge
    send_dear_johms.c done

---
+++ 7CH/send_dear_johns.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+enum response_type {DUMP, SECOND_CHANCE, MARRIAGE};
+typedef struct {
+    char *name;
+    enum response_type type;
+}response;
+
+void dump(response r)
+{
+    printf("Dear %s,\n", r.name);
+    puts("Unforturnately your last data contacted us to");
+    puts("say that they will not be seeing you again");
+}
+
+void second_chance(response r)
+{
+    printf("Dear %s,\n", r.name);
+    puts("Good nwesL your last data has asked us to");
+    puts("arrange another meeting. Please call ASAP");
+}
+
+void marriage(response r)
+{
+    printf("Dear %s,\n", r.name);
+    puts("Congraulations! Your last data has contacted");
+    puts("us with a proposal of marriage");
+}
+
+int main()
+{
+response r[]={
+     {"Mike", DUMP}, {"Luis", SECOND_CHANCE}, {"Matt", SECOND_CHANCE},
+     {"William", MARRIAGE}
+    };
+    int i;
+    for (i=0; i<4; i++){
+        switch(r[i].type){
+        case DUMP:
+            dump(r[i]);
+            break;
+        case SECOND_CHANCE:
+            second_chance(r[i]);
+            break;
+        default:
+            marriage(r[i]);
+        }
+    }
+    return 0;
+}
